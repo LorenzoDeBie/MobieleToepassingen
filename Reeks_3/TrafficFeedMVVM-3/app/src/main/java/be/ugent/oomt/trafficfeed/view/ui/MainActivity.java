e7e42fea798dbbf5d9ca7d9e61c6b94a20a4f51c
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2019-12-27 13:56:04 +0100

    Lab 5: 1 Project set-up: internet permissions

--- Reeks_3/TrafficFeedMVVM-3/app/src/main/java/be/ugent/oomt/trafficfeed/view/ui/MainActivity.java
+++ Reeks_3/TrafficFeedMVVM-3/app/src/main/java/be/ugent/oomt/trafficfeed/view/ui/MainActivity.java
@@ -29,8 +29,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             this.navController = Navigation.findNavController(this, R.id.navhostfragment);
             NavigationUI.setupActionBarWithNavController(this, this.navController);
-        } catch (IllegalArgumentException e) {
-        }
+        } catch (IllegalArgumentException ignored) { }
     }
 
     @Override
